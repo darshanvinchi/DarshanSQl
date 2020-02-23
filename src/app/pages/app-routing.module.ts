cec731c30f0f4332d5892b0b1437d5e0138d0fcb
darshanvinchi <52873499+darshanvinchi@users.noreply.github.com>
2020-02-23 10:49:10 +0530

    Add files via upload

---
+++ src/app/pages/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'developers', pathMatch: 'full' },
+  { path: 'developers', loadChildren: () => import('./pages/developers/developers.module').then( m => m.DevelopersPageModule)},
+  // {
+  //   path: 'developers',
+  //   loadChildren: () => import('./pages/developers/developers.module').then( m => m.DevelopersPageModule)
+  // },
+  {
+    path: 'developers/:id',
+    loadChildren: () => import('./pages/developer/developer.module').then( m => m.DeveloperPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
